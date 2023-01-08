878e1d35e020b369ca1b8f25b2f6129ad024169d
jls83 <josephlsanders@gmail.com>
2023-01-07 22:43:20 -0500

    Use class rather than dict for pattern matching

--- py_box/corner.py
+++ py_box/corner.py
@@ -18,16 +18,28 @@ class CornerException(Exception):
     pass
 
 
-class Corner:
-    DEFAULT_LINE_WEIGHTS = {
-        KEY_HORIZONTAL: LineWeight.LIGHT,
-        KEY_VERTICAL: LineWeight.LIGHT,
-    }
+class CornerLineWeights:
+    def __init__(self, horizontal, vertical):
+        self.horizontal = horizontal
+        self.vertical = vertical
 
-    DEFAULT_LINE_PATTERNS = {
-        KEY_HORIZONTAL: LinePattern.SOLID,
-        KEY_VERTICAL: LinePattern.SOLID,
-    }
+
+class CornerLinePatterns:
+    def __init__(self, horizontal, vertical):
+        self.horizontal = horizontal
+        self.vertical = vertical
+
+
+class Corner:
+    DEFAULT_LINE_WEIGHTS = CornerLineWeights(
+        LineWeight.LIGHT,
+        LineWeight.LIGHT,
+    )
+
+    DEFAULT_LINE_PATTERNS = CornerLinePatterns(
+        LinePattern.SOLID,
+        LinePattern.SOLID,
+    )
 
     def __init__(self, direction: CornerDirection, line_weights=DEFAULT_LINE_WEIGHTS,
                  line_patterns=DEFAULT_LINE_PATTERNS):
@@ -42,65 +54,49 @@ class Corner:
     def unicode_name(self):
         parts = ["BOX DRAWINGS"]
 
-        # TODO: use match!
-        vertical_direction = horizontal_direction = ""
-        if self.direction == CornerDirection.NORTH_WEST:
-            vertical_direction = "DOWN"
-            horizontal_direction = "RIGHT"
-        elif self.direction == CornerDirection.NORTH_EAST:
-            vertical_direction = "DOWN"
-            horizontal_direction = "LEFT"
-        elif self.direction == CornerDirection.SOUTH_EAST:
-            vertical_direction = "UP"
-            horizontal_direction = "LEFT"
-        elif self.direction == CornerDirection.SOUTH_WEST:
-            vertical_direction = "UP"
-            horizontal_direction = "RIGHT"
+        match self.direction:
+            case CornerDirection.NORTH_WEST:
+                vertical_direction, horizontal_direction = "DOWN", "RIGHT"
+            case CornerDirection.NORTH_EAST:
+                vertical_direction, horizontal_direction = "DOWN", "LEFT"
+            case CornerDirection.SOUTH_EAST:
+                vertical_direction, horizontal_direction = "UP", "LEFT"
+            case CornerDirection.SOUTH_WEST:
+                vertical_direction, horizontal_direction = "UP", "RIGHT"
+            # Because we match against the enum cases, we should never get here!
+            case _:
+                raise Exception
 
-        # check if the weights are the same; we only want one if so.
-        if self.line_weights[KEY_HORIZONTAL] == self.line_weights[KEY_VERTICAL]:
-            parts.extend([
-                self.line_weights[KEY_VERTICAL].name,
-                vertical_direction,
-                "AND",
-                horizontal_direction,
-            ])
-        elif self.line_weights[KEY_VERTICAL] == LineWeight.DOUBLE and self.line_weights[KEY_HORIZONTAL] == LineWeight.LIGHT:
-            parts.extend([
-                vertical_direction,
-                self.line_weights[KEY_VERTICAL].name,
-                "AND",
-                horizontal_direction,
-                "SINGLE"
-            ])
-        elif self.line_weights[KEY_HORIZONTAL] == LineWeight.DOUBLE and self.line_weights[KEY_VERTICAL] == LineWeight.LIGHT:
-            parts.extend([
-                vertical_direction,
-                "SINGLE",
-                "AND",
-                horizontal_direction,
-                self.line_weights[KEY_HORIZONTAL].name,
-            ])
-        else:
-            parts.extend([
-                vertical_direction,
-                self.line_weights[KEY_VERTICAL].name,
-                "AND",
-                horizontal_direction,
-                self.line_weights[KEY_HORIZONTAL].name,
-            ])
+        match self.line_weights:
+            # check if the weights are the same; we only want one if so.
+            case CornerLineWeights(horizontal=h, vertical=v) if h == v:
+                chg = [v.name, vertical_direction, "AND", horizontal_direction]
+            # if we have a DOUBLE weight and a LIGHT weight, change the LIGHT name to "SINGLE"
+            case CornerLineWeights(horizontal=LineWeight.DOUBLE, vertical=LineWeight.LIGHT):
+                chg = [vertical_direction, LineWeight.DOUBLE.name, "AND", horizontal_direction, "SINGLE"]
+            # Same here
+            case CornerLineWeights(horizontal=LineWeight.LIGHT, vertical=LineWeight.DOUBLE):
+                chg = [vertical_direction, "SINGLE", "AND", horizontal_direction, LineWeight.DOUBLE.name]
+            # The "regular" case
+            case CornerLineWeights(horizontal=h, vertical=v):
+                chg = [vertical_direction, v.name, "AND", horizontal_direction, h.name]
+            # Because we match against the enum cases, we should never get here!
+            case _:
+                raise Exception
+
+        parts.extend(chg)
 
         return " ".join(parts)
 
     @classmethod
     def from_line_appearance(cls, direction: CornerDirection, horizontal: LineAppearance,
                              vertical: LineAppearance):
-        line_weights = {
-            KEY_HORIZONTAL: horizontal.line_weight,
-            KEY_VERTICAL: vertical.line_weight,
-        }
-        line_patterns = {
-            KEY_HORIZONTAL: horizontal.line_pattern,
-            KEY_VERTICAL: vertical.line_pattern,
-        }
+        line_weights = CornerLineWeights(
+            horizontal=horizontal.line_weight,
+            vertical=vertical.line_weight,
+        )
+        line_patterns = CornerLinePatterns(
+            horizontal=horizontal.line_pattern,
+            vertical=vertical.line_pattern,
+        )
         return cls(direction, line_weights, line_patterns)
